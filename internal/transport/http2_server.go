4a757fb818821262a182fc349192d60171710943
rainayangg <raina.yang.chn@gmail.com>
2026-06-02 21:32:23 -0600

    Test KOMA handler goroutine handoff

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -965,7 +965,6 @@ func (t *http2Server) HandleStreamsKoma(ctx context.Context, komafd int, handle 
 		}
 	}
 	events := make([]unix.EpollEvent, 16)
-	handlerSlots := make(chan struct{}, 1)
 
 	koma.KomaPull(komafd)
 	for {
@@ -1055,11 +1054,12 @@ func (t *http2Server) HandleStreamsKoma(ctx context.Context, komafd int, handle 
 		// fed into the `operateHeaders` will run, and either i) spawn a new go routine to call handleStream and process
 		// the associated stream (which involves blocking and waiting), ii) assign a go-routine worker to do the associated work.
 		if ifNewStream {
-			handlerSlots <- struct{}{}
+			done := make(chan struct{})
 			go func(stream *ServerStream) {
-				defer func() { <-handlerSlots }()
 				handle(stream)
+				close(done)
 			}(stream)
+			<-done
 		}
 
 	}
