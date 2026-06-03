f2c4830340d91cdfb2c9a0f1041b388234f74d8b
rainayangg <raina.yang.chn@gmail.com>
2026-06-02 21:22:33 -0600

    Cap KOMA async handlers per fd

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -965,6 +965,7 @@ func (t *http2Server) HandleStreamsKoma(ctx context.Context, komafd int, handle 
 		}
 	}
 	events := make([]unix.EpollEvent, 16)
+	handlerSlots := make(chan struct{}, 1)
 
 	koma.KomaPull(komafd)
 	for {
@@ -1054,8 +1055,11 @@ func (t *http2Server) HandleStreamsKoma(ctx context.Context, komafd int, handle 
 		// fed into the `operateHeaders` will run, and either i) spawn a new go routine to call handleStream and process
 		// the associated stream (which involves blocking and waiting), ii) assign a go-routine worker to do the associated work.
 		if ifNewStream {
-			// go handle(stream)
-			handle(stream)
+			handlerSlots <- struct{}{}
+			go func(stream *ServerStream) {
+				defer func() { <-handlerSlots }()
+				handle(stream)
+			}(stream)
 		}
 
 	}
