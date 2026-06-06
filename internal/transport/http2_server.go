d2454ec46f3b82b6c76212a2b3d9e299a2c65583
rainayangg <raina.yang.chn@gmail.com>
2026-06-06 06:10:38 -0600

    Revert "Lower KOMA TX throttle limit"
    This reverts commit ec32025969f440714d5abd6f6bba3cc5a98979a7.

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -80,7 +80,7 @@ var komaDoneEventFDWake = [8]byte{1}
 var komaDebugStatsEnabled = os.Getenv("GRPC_KOMA_DEBUG_STATS") != ""
 
 const (
-	komaTxThrottleLimit  = 5
+	komaTxThrottleLimit  = 10
 	komaRXActorsPerFD    = 2
 	komaDebugHistBuckets = 65
 )
