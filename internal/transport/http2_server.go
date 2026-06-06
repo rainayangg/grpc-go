ec0fff218e62af6284c4f8f6d91345e532829729
rainayangg <raina.yang.chn@gmail.com>
2026-06-06 13:28:07 -0600

    Raise Koma TX throttle limit

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -80,7 +80,7 @@ var komaDoneEventFDWake = [8]byte{1}
 var komaDebugStatsEnabled = os.Getenv("GRPC_KOMA_DEBUG_STATS") != ""
 
 const (
-	komaTxThrottleLimit  = 4
+	komaTxThrottleLimit  = 20
 	komaRXActorsPerFD    = 2
 	komaDebugHistBuckets = 65
 )
