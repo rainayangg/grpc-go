eef74dc6eaefefb4f03319ef174008cf9d0b8cd4
rainayangg <raina.yang.chn@gmail.com>
2026-06-06 05:55:29 -0600

    Increase KOMA RX actors per fd

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -81,7 +81,7 @@ var komaDebugStatsEnabled = os.Getenv("GRPC_KOMA_DEBUG_STATS") != ""
 
 const (
 	komaTxThrottleLimit  = 10
-	komaRXActorsPerFD    = 2
+	komaRXActorsPerFD    = 3
 	komaDebugHistBuckets = 65
 )
 
